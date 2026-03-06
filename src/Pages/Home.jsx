85445ffd447b46a5e39603891a85914fec1e84be
Rishi Bagora <codzenrb@gmail.com>
2026-03-06 12:09:13 +0530

    fixed import path

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import Navbar from '../components/home/Navbar'
-import Hotelname from '../components/home/HotelName'
+import HotelName from '../components/home/HotelName'
 import Background from '../components/home/Background'
 import Aboutsection from '../components/home/Aboutsection'
 import RoomsAndSuites from '../components/home/RoomsAndSuites'
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <div className='w-screen  bg-[#faf9f6]'>
         {/* <Navbar/> */}
-        <Hotelname/>
+        <HotelName/>
         <Background/>
         <Aboutsection/> 
         <LocationSection/>
